e395872eab739287562dadc9ca263e671a869166
Marc Mills <mdmills1990@gmail.com>
2019-07-10 13:44:16 -0600

    adding authentication logic so that when my fetch request to the api actually works it'll be ready to set a session for current user..

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,5 +1,15 @@
-class SessionsController < ApplicationController
+class Api::V1::SessionsController < ApplicationController
+
   def create
+    @user = User.find_by(email: params[:session][:email])
+    if @user && @user.authenticate(params[:session][:password])
+      session[:user_id] = @user.id
+      render json: @user
+    else
+      render json: {
+        error: "Invalid Credentials"
+      }
+    end
   end
-  
+
 end
