11fb2d22bb9b9a528bf88d94f87259409848738f
Marc Mills <mdmills1990@gmail.com>
2019-07-11 13:27:01 -0600

    testing variables nothings actually different

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,6 +2,7 @@ class Api::V1::SessionsController < ApplicationController
 
   def create
     @user = User.find_by(email: params[:session][:email])
+
     if @user && @user.authenticate(params[:session][:password])
       session[:user_id] = @user.id
       render json: @user
@@ -12,4 +13,14 @@ class Api::V1::SessionsController < ApplicationController
     end
   end
 
+
+  def get_current_user
+    if logged_in?
+      render json: current_user
+    else
+      render json: {
+        error: "No one logged in."
+      }
+    end
+  end
 end
