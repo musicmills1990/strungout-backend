c37a40727fa9daa7e5810f63dba8a67c0dc69c13
Marc Mills <mdmills1990@gmail.com>
2019-07-16 09:57:48 -0600

    removes custom attribute logic since I'm pulling that into the store separately

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,14 +1,14 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
   attributes :name, :email, :player_type
-  attribute :guitars do |user|
-    user.guitars.map do |guitar|
-      {
-        guitar_type: guitar.guitar_type,
-        brand: guitar.brand,
-        model: guitar.model,
-        color: guitar.color,
-      }
-    end
-  end
+  # attribute :guitars do |user|
+  #   user.guitars.map do |guitar|
+  #     {
+  #       guitar_type: guitar.guitar_type,
+  #       brand: guitar.brand,
+  #       model: guitar.model,
+  #       color: guitar.color,
+  #     }
+  #   end
+  # end
 end
