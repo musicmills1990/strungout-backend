91a6f67e0bf9cc31b9e535dbc70760930ab00bef
Marc Mills <mdmills1990@gmail.com>
2019-07-16 13:50:16 -0600

    adds validations for uniqueness of email

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   has_secure_password
   has_many  :guitars
   has_many :string_packs
+  validates :name, :email, :address, presence: true
+  validates :email, uniqueness: true
+
 end
