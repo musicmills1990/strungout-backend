d8ce571adb9d76c6d936b90e83bd9f1053605050
Marc Mills <mdmills1990@gmail.com>
2019-07-17 15:24:46 -0600

    changed guitar routes to try and debug bad request 400

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
   post '/api/v1/login', to: 'api/v1/sessions#create'
   post '/api/v1/signup', to: 'api/v1/users#create'
+  post '/api/v1/guitars', to: 'api/v1/guitars#create'
+  get '/api/v1/guitars', to: 'api/v1/guitars#index'
   get '/api/v1/get_current_user', to: 'api/v1/sessions#get_current_user'
   delete '/api/v1/logout', to: 'api/v1/sessions#destroy'
 
   namespace :api do
     namespace :v1 do
       resources :users
-      resources :guitars
       resources :string_packs
     end
   end
