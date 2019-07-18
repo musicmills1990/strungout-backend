1835ddae19f94d8e2451ba9479eae5407099449f
Marc Mills <mdmills1990@gmail.com>
2019-07-18 13:10:05 -0600

    adds counters to the whitelisted data going through JSON

--- app/serializers/string_pack_serializer.rb
+++ app/serializers/string_pack_serializer.rb
@@ -1,4 +1,5 @@
 class StringPackSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :guage, :brand, :model, :price
+  attributes :guage,
+  :brand, :model, :price, :low_e_string_counter, :a_string_counter, :d_string_counter, :g_string_counter, :b_string_counter, :high_e_string_counter
 end
