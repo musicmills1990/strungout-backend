3e89fa8c3b2544d9aac640654d74cc393e2c0f66
Marc Mills <mdmills1990@gmail.com>
2019-07-19 15:15:33 -0600

    built an instance method to increase the string_counters, but I'm doing that on the frontend before the request so it's commented out

--- app/models/string_pack.rb
+++ app/models/string_pack.rb
@@ -2,22 +2,22 @@ class StringPack < ApplicationRecord
   belongs_to :user
 
 
-  def update_string_counter
-    # the code I wish I had.....
-    # not sure how I get the update params value
-    if stringpack_params === "Low E"
-      self.low_e_string_counter =+ 1
-    elsif stringpack_params === "A"
-      self.a_string_counter =+ 1
-    elsif stringpack_params === "D"
-      self.a_string_counter =+ 1
-    elsif stringpack_params === "G"
-      self.g_string_counter =+ 1
-    elsif stringpack_params === "B"
-      self.b_string_counter =+ 1
-    elsif stringpack_params === "High E"
-      self.high_e_string_counter =+ 1
-    end
-  end
-  
+  # def update_string_counter
+  #   # the code I wish I had.....
+  #   # not sure how I get the update params value
+  #   if stringpack_params === "Low E"
+  #     self.low_e_string_counter =+ 1
+  #   elsif stringpack_params === "A"
+  #     self.a_string_counter =+ 1
+  #   elsif stringpack_params === "D"
+  #     self.a_string_counter =+ 1
+  #   elsif stringpack_params === "G"
+  #     self.g_string_counter =+ 1
+  #   elsif stringpack_params === "B"
+  #     self.b_string_counter =+ 1
+  #   elsif stringpack_params === "High E"
+  #     self.high_e_string_counter =+ 1
+  #   end
+  # end
+
 end
