a7dffdbacb5663a505cba139e52833d4fa39e1f3
Marc Mills <mdmills1990@gmail.com>
2019-07-19 15:15:59 -0600

    adds new stringPack for a user to experiment with BrokenStringMessage function stringName()

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,4 +9,4 @@
 # Guitar.create(guitar_type: "accoustic", brand: "gibson", model: 'CD-140SCE', color: "red", user_id: 1)
 # StringPack.create(guage: 'light', brand: "D'addario", model: "XL110", price: 5.50, user_id: 1)
 # User.create(name: 'Asia', email: "asia@asia.com", password: "password", address: "123 Street st. New York NY 10030", player_type: "outdoor festival performer", plan: "premium", pay_schedule: "monthly")
-StringPack.create(guage: 'medium', brand: "Martin", model: "SP bronze", price: 7.50, user_id: 7)
+StringPack.create(guage: 'medium', brand: "Martin", model: "SP bronze", price: 7.50, low_e_string_counter: 4, a_string_counter: 4, d_string_counter: 4, g_string_counter: 7, b_string_counter: 4, high_e_string_counter: 4, user_id: 10)
