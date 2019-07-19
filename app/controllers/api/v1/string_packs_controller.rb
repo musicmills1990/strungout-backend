e43d4013a5c95b994fa0dc00141cfffaab138b39
Marc Mills <mdmills1990@gmail.com>
2019-07-19 12:53:37 -0600

    stubs out update controller to receive a PATCH request

--- app/controllers/api/v1/string_packs_controller.rb
+++ app/controllers/api/v1/string_packs_controller.rb
@@ -12,19 +12,12 @@ class Api::V1::StringPacksController < ApplicationController
     end
   end
 
-  # GET /stringpacks/1
-  def show
-    # render json: @stringpack
-    stringpack_json = StringPackSerializer.new(@stringpack).serialized_json
-    render json: stringpack_json
-  end
-
   # POST /stringpacks
   def create
     @stringpack = StringPack.new(stringpack_params)
     if @stringpack.save
-      session[:user_id] = @guitar.user_id
-      render json: @stringpack, status: :created, location: @stringpack
+      session[:user_id] = @stringpack.user_id
+      render json: StringPackSerializer.new(@stringpacks), status: created
     else
       render json: @stringpack.errors, status: :unprocessable_entity
     end
@@ -33,15 +26,11 @@ class Api::V1::StringPacksController < ApplicationController
   # PATCH/PUT /stringpacks/1
   def update
     if @stringpack.update(stringpack_params)
-      render json: @stringpack
+      @stringpack.update_string_counter
+      render json: StringPackSerializer.new(@stringpacks), status: updated
     else
       render json: @stringpack.errors, status: :unprocessable_entity
     end
-  end
-
-  # DELETE /stringpacks/1
-  def destroy
-    @stringpack.destroy
   end
 
   private
