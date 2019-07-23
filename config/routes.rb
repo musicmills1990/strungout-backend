7b33874542582b59c593b737574de32c15622e07
Marc Mills <mdmills1990@gmail.com>
2019-07-23 15:40:58 -0600

    changed guitar route back to a resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   post '/api/v1/login', to: 'api/v1/sessions#create'
   post '/api/v1/signup', to: 'api/v1/users#create'
-  get '/api/v1/guitars', to: 'api/v1/guitars#index'
-  post '/api/v1/guitars', to: 'api/v1/guitars#create'
+  # get '/api/v1/guitars', to: 'api/v1/guitars#index'
+  # post '/api/v1/guitars', to: 'api/v1/guitars#create'
   get '/api/v1/get_current_user', to: 'api/v1/sessions#get_current_user'
   delete '/api/v1/logout', to: 'api/v1/sessions#destroy'
 
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users
       resources :string_packs
+      resources :guitars
     end
   end
 end
